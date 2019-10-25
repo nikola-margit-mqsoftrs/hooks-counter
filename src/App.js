013ab339960cba6f148c4fb62da3822fa39cee89
ml250271 <lukicmilica@hotmail.com>
2019-10-25 16:55:22 +0200

    add input for entering increment

--- src/App.js
+++ src/App.js
@@ -2,12 +2,18 @@ import React, { useState } from "react"
 
 function App() {
     const [count, setCount] = useState(0)
+    const [increment, setIncrement] = useState(1)
+
+    const handleIncrement = ({ target }) => {
+        setIncrement(Number(target.value))
+    }
 
     return (
         <>
-            <button onClick={() => setCount(count - 1)}>-</button>
+            <button onClick={() => setCount(count - increment)}>-</button>
             {count}
-            <button onClick={() => setCount(count + 1)}>+</button>
+            <button onClick={() => setCount(count + increment)}>+</button>
+            <input type="number" onChange={handleIncrement} value={increment} />
         </>
     )
 }
